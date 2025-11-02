10ee016ff5cccd57ad0b585ba36a2eb802c9de59
Md Imtiaj Uddin Chowdhury <imtiajchowdhury333@gmail.com>
2025-11-02 13:48:22 +0600

    Add files via upload

---
+++ instra login/a.js
@@ -0,0 +1,25 @@
+const btn = document.getElementById('bnn')
+btn.addEventListener("click", () => {
+    console.log('connected')
+
+    window.location.href = "./a.html"
+
+})
+
+const dtn = document.getElementById('cnn')
+btn.addEventListener("click", () => {
+    console.log('connected')
+
+    window.location.href = "./a.html"
+
+})
+const ctn = document.getElementById('dnn')
+btn.addEventListener("click", () => {
+    console.log('connected')
+
+    window.location.href = "./a.html"
+
+})
+
+
+
